df9d13726186ba86a55c5b06b17ece712eb3a2ed
Omer Daniel <omerda@gmail.com>
2020-02-03 21:21:14 +0200

    added models, fix settings.py

--- mymima/models.py
+++ mymima/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 class Artists(models.Model):
-    pass
+    name = models.CharField(max_length=200)
 
 
+class Songs(models.Model):
+    artist_id = models.ForeignKey(Artists)
+    name = models.CharField(max_length=200)
