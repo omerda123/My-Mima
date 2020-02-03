2daac40745b97580d7c546e95244982191d9c40f
Omer Daniel <omerda@gmail.com>
2020-02-03 21:49:30 +0200

    create model, connected to pg db

--- facts/models.py
+++ facts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Artists(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Songs(models.Model):
+    artist_id = models.ForeignKey(Artists, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+class facts(models.Model):
+    song_id = models.ForeignKey(Songs, on_delete=models.CASCADE)
+    fact = models.TextField()
--- facts/urls.py
+++ facts/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+
+app_name = "facts"
 
 urlpatterns = [
-    path('', include('facts.urls')),
+    path('', views.index, name="index"),
 ]
