3d024aa35cfd89a17bb55e37d40ff8df959a8f72
Omer Daniel <omerda@gmail.com>
2020-02-08 13:35:23 +0200

    fix tests closes #5

--- facts/tests.py
+++ facts/tests.py
@@ -7,32 +7,26 @@ from .models import Fact
 
 
 class QuestionModelTests(TestCase):
-    def test_add_artist(self):
-        king = Artist(name="King Crimson")
-        self.assertEqual(king.name, "King Crimson")
-
-    def test_add_song(self):
-        king = Artist(name="King Crimson")
-        epitaph = Song(name="Epitaph", artist_id=king)
-        self.assertEqual(king.name, "King Crimson")
-        self.assertEqual(epitaph.name, "Epitaph")
-        self.assertEqual(epitaph.artist_id.name, "King Crimson")
+    def setUp(self):
+        king = Artist.objects.create(name="King Crimson", mima_id=1)
+        epitaph = Song.objects.create(name="Epitaph", artist=king, mima_id=2)
+        fact = Fact.objects.create(song=epitaph,
+                                   fact="This song title as well as the lyrics of this song refer to the message that is displayed on a gravestone. In this song, the singer is facing a struggle and fears that his epitaph will be \"confusion.\" Greg Lake, who was the bass player with the group for the In the Court of The Crimson King album (his only album with the band, as he left to form Emerson, Lake & Palmer), explained: 'Epitaph' is basically a song about looking with confusion upon a world gone mad. King Crimson had a strange ability to write about the future in an extremely prophetic way and the messages this song contains are even more relative today than they were when the song was originally written.",
+                                   author="Omer Daniel",
+                                   date_created=timezone.now())
 
     def test_add_fact(self):
-        king = Artist(name="King Crimson")
-        epitaph = Song(name="Epitaph", artist_id=king)
-        fact = Fact(song_id=epitaph,
-                    fact="This song title as well as the lyrics of this song refer to the message that is displayed on a gravestone. In this song, the singer is facing a struggle and fears that his epitaph will be \"confusion.\" Greg Lake, who was the bass player with the group for the In the Court of The Crimson King album (his only album with the band, as he left to form Emerson, Lake & Palmer), explained: 'Epitaph' is basically a song about looking with confusion upon a world gone mad. King Crimson had a strange ability to write about the future in an extremely prophetic way and the messages this song contains are even more relative today than they were when the song was originally written.",
-                    author="Omer Daniel",
-                    date_created=timezone.now())
+        artist = Artist.objects.get(name="King Crimson")
+        song = Song.objects.get(name="Epitaph")
+        fact = Fact.objects.get(author="Omer Daniel")
 
-        self.assertEqual(king.name == "King Crimson")
-        self.assertEqual(epitaph.name, "Epitaph")
-        self.assertEqual(epitaph.artist_id.name, "King Crimson")
+        self.assertEqual(artist.name , "King Crimson")
+        self.assertEqual(song.name, "Epitaph")
+        self.assertEqual(song.artist.name, "King Crimson")
         self.assertEqual(
             fact.fact,
             "This song title as well as the lyrics of this song refer to the message that is displayed on a gravestone. In this song, the singer is facing a struggle and fears that his epitaph will be \"confusion.\" Greg Lake, who was the bass player with the group for the In the Court of The Crimson King album (his only album with the band, as he left to form Emerson, Lake & Palmer), explained: 'Epitaph' is basically a song about looking with confusion upon a world gone mad. King Crimson had a strange ability to write about the future in an extremely prophetic way and the messages this song contains are even more relative today than they were when the song was originally written.",
         )
-        self.assertEqual(fact.song_id.name, "Epitaph")
-        self.assertEqual(fact.song_id.artist_id.name, "King Crimson")
+        self.assertEqual(fact.song.name, "Epitaph")
+        self.assertEqual(fact.song.artist.name, "King Crimson")
         self.assertEqual(fact.author, "Omer Daniel")
